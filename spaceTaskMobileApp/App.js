5f4d2332821b0879f599f0f97583a0bf0f16beb1
gidi2345 <gidon1smila@gmail.com>
2022-01-10 12:29:52 +0200

    rearrange

---
+++ spaceTaskMobileApp/App.js
@@ -0,0 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import ContactsManager from "./src/components/Contacts";
+import MissionCard from "./src/components/Cards";
+import MissionCreationForm from "./src/components/MissionCreationForm";
+
+const Section = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const Missions = [
+    {
+        uuid: 'uuid-test-1',
+        title: 'חלוקת אוכל',
+        description: 'מחלקים אוכל',
+        timeToComplete: 32,
+        locations:[{x:2, y:3}],
+        locationsAsString:['dsd','sds']
+    }
+]
+
+const App =  ()  => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View>
+       {/* <MissionCard title={'חלוקת מזון'} description={'dsfds'} locations={'df'} timeToComplete={50}/>*/}
+        <MissionCreationForm />
+        {/*<ContactsManager />*/}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
